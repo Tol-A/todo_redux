280f7a4c6155e4e5039c8501c92bbbd1880f85a1
Tol-A <atol4eev@yandex.ru>
2023-10-14 08:37:46 +0300

    fix rtc

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoList from './redux/features/MyTodo/TodoList';
 function App() {
   return (
     <div className="App">
+      <h1>Todo List</h1>
         <TodoForm/>
         <TodoList/>
     </div>
